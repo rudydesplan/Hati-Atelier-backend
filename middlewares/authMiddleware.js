7027b0a22f93706f0861ad45715d00dc0494f3bc
rudydesplan <desrudy@hotmail.com>
2023-10-12 13:11:23 +0200

    comments

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,19 +1,38 @@
+// Import the JWT package to handle JSON Web Tokens
 const jwt = require('jsonwebtoken');
 
+// Middleware to authenticate a user based on JWT token in headers
 const authenticate = (req, res, next) => {
+  // Extract the token from the Authorization header
   const token = req.headers.authorization;
+
+  // If no token is found, return a 401 (Unauthorized) status
   if (!token) return res.status(401).send('Access denied. No token provided.');
 
+  // Verify the received token using JWT package
   jwt.verify(token, 'secret', (err, decoded) => {
+    // If token is invalid, return a 401 (Unauthorized) status
     if (err) return res.status(401).send('Invalid token.');
+    
+    // Attach the decoded payload to the request object
     req.user = decoded;
+
+    // Proceed to the next middleware or route handler
     next();
   });
 };
 
+// Middleware to authorize user as admin
 const authorizeAdmin = (req, res, next) => {
-  if (req.user.role !== 'admin') return res.status(403).send('Access forbidden. Admins only.');
+  // Check if the user role is 'admin'
+  if (req.user.role !== 'admin') {
+    // If not admin, return a 403 (Forbidden) status
+    return res.status(403).send('Access forbidden. Admins only.');
+  }
+
+  // If user is admin, proceed to the next middleware or route handler
   next();
 };
 
+// Export the middlewares for use in other files
 module.exports = { authenticate, authorizeAdmin };
--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,28 +1,41 @@
+// Import all the Custom Errors and the logging configuration
 const { CustomError, BadRequestError, UnauthorizedError, ForbiddenError, NotFoundError, InternalServerError, MethodNotAllowedError, NotAcceptableError, PreconditionFailedError, PayloadTooLargeError, URITooLongError, UnsupportedMediaTypeError, UnprocessableEntityError, TooManyRequestsError, NotImplemented, BadGateway, ServiceUnavailable, GatewayTimeout, InsufficientStorage } = require('../errors/CustomErrors');
 const logger = require('../utils/winstonConfig');
 
+// Error-handling middleware function
 module.exports = function (err, req, res, next) {
+  // Prepare the default error response
   let errorResponse = {
-    message: 'An unexpected error occurred',
-    details: err.message,
-    timestamp: new Date().toISOString(),
-    method: req.method,
-    url: req.originalUrl
+    message: 'An unexpected error occurred',  // Default message
+    details: err.message,                     // Detailed message from error object
+    timestamp: new Date().toISOString(),       // Current time
+    method: req.method,                        // HTTP Method
+    url: req.originalUrl                      // URL of the request
   };
 
+  // Prepare the log object for storing detailed information
   let logObject = {
-    ...errorResponse,
-    stack: err.stack
+    ...errorResponse,  // Spread in all fields from errorResponse
+    stack: err.stack   // Stack trace for debugging
   };
 
-  // Check if the error is a CustomError or its subclass
+  // Check if the error is a type of CustomError
   if (err instanceof CustomError) {
+    // Update the default message with specific error message
     errorResponse.message = err.message;
+
+    // Send response back to the client with appropriate status code
     res.status(err.statusCode).send({ error: errorResponse });
+
+    // Log a warning message
     logger.warn(JSON.stringify(logObject));
   } 
+  // For all other types of errors
   else {
+    // Send a generic 500 Internal Server Error to the client
     res.status(500).send({ error: errorResponse });
+
+    // Log the error as a severe issue
     logger.error(JSON.stringify(logObject));
   }
 };
